cea416b16a5916381cb2270142651dc487ad078b
christine gomez <christine.gomez1226@gmail.com>
2018-09-06 13:53:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence)
   array = []
   array<<sentence.split
   array.collect {|word| word.reverse}
-
-  array.each do {|word| word.reverse}
+end
+  array.each do |word|
+    word.reverse
   end
 end
